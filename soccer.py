8bdde9fd5c04df189208e32a702af0d2c2de0d63
Piotr Tutak <p.tutak@outlook.com>
2022-05-14 13:25:54 +0200

    most wins per month in year

--- soccer.py
+++ soccer.py
@@ -13,6 +13,20 @@ def main(soccer_file: str):
     session = SparkSession.builder.appName("soccer analysis").getOrCreate()
     soccer_df = session.read.csv(soccer_file, sep=",", header=True, inferSchema=True)
     soccer_df.createOrReplaceTempView("soccer")
+
+    most_wins_in_the_month_df = session.sql(
+        """
+            SELECT home_team, MAX(anon_1.win_number + COALESCE(anon_2.win_number, 0)) as max_win_number FROM
+            (select home_team, MONTH(match_date) as match_month, YEAR(match_date) as match_year, COUNT(1) AS win_number
+            from soccer WHERE home_score > away_score GROUP BY home_team, MONTH(match_date), YEAR(match_date)) AS anon_1
+            JOIN
+            (SELECT away_team, MONTH(match_date) as match_month, YEAR(match_date) as match_year, COUNT(1) AS win_number
+            FROM soccer WHERE away_score > home_score GROUP BY away_team, MONTH(match_date), YEAR(match_date)) AS anon_2
+            ON home_team = away_team and anon_1.match_month = anon_2.match_month and anon_1.match_year = anon_2.match_year
+        """
+    )
+    most_wins_in_the_month_df.show()
+
     home_win_rate_df = session.sql(
         """
             SELECT home_team, MAX(home_win_rate) FROM
@@ -36,6 +50,8 @@ def main(soccer_file: str):
         """
     )
     away_win_rate_df.show()
+
+
 
 
 if __name__ == "__main__":
