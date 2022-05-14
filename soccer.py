db879909c3d2ba258d8d877a63e2582a5d4e873f
Piotr Tutak <p.tutak@outlook.com>
2022-05-14 13:03:52 +0200

    max win rates

--- soccer.py
+++ soccer.py
@@ -1,5 +1,3 @@
-from math import inf
-
 import click
 from pyspark.sql import SparkSession
 
@@ -15,7 +13,29 @@ def main(soccer_file: str):
     session = SparkSession.builder.appName("soccer analysis").getOrCreate()
     soccer_df = session.read.csv(soccer_file, sep=",", header=True, inferSchema=True)
     soccer_df.createOrReplaceTempView("soccer")
-    result_df = session.sql("SELECT ")
+    home_win_rate_df = session.sql(
+        """
+            SELECT home_team, MAX(home_win_rate) FROM
+            (SELECT anon_1.home_team, CAST(win_number as DOUBLE) / CAST(home_matches + away_matches AS DOUBLE) AS home_win_rate FROM
+            (select home_team, COUNT(1) AS win_number from soccer WHERE home_score > away_score GROUP BY home_team) as anon_1
+            JOIN (SELECT away_team, COUNT(1) as away_matches FROM soccer GROUP BY away_team) as anon_2
+            JOIN (SELECT home_team, COUNT(1) AS home_matches FROM soccer GROUP BY home_team) AS anon_3
+            on anon_1.home_team = anon_2.away_team AND anon_1.home_team = anon_3.home_team);
+        """
+    )
+    home_win_rate_df.show()
+
+    away_win_rate_df = session.sql(
+        """
+            SELECT away_team, MAX(away_win_rate) FROM
+            (SELECT anon_1.away_team, CAST(win_number as DOUBLE) / CAST(home_matches + away_matches AS DOUBLE) AS away_win_rate FROM
+            (select away_team, COUNT(1) AS win_number from soccer WHERE away_score > home_score GROUP BY away_team) as anon_1
+            JOIN (SELECT away_team, COUNT(1) as away_matches FROM soccer GROUP BY away_team) as anon_2
+            JOIN (SELECT home_team, COUNT(1) AS home_matches FROM soccer GROUP BY home_team) AS anon_3
+            on anon_1.away_team = anon_2.away_team AND anon_1.away_team = anon_3.home_team);
+        """
+    )
+    away_win_rate_df.show()
 
 
 if __name__ == "__main__":
